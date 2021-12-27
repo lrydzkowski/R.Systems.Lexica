b847466e0cc3d303aba007d6674461322d3b92b1
Łukasz Rydzkowski <lukasz_rydzkowski@loco.pl>
2021-12-27 19:05:20 +0100

    refactor: file-scoped namespace

--- R.Systems.Lexica.WebApi/Controllers/SetController.cs
+++ R.Systems.Lexica.WebApi/Controllers/SetController.cs
@@ -4,42 +4,41 @@ using R.Systems.Lexica.Core.Models;
 using R.Systems.Lexica.Core.Services;
 using R.Systems.Shared.Core.Validation;
 
-namespace R.Systems.Lexica.WebApi.Controllers
+namespace R.Systems.Lexica.WebApi.Controllers;
+
+[ApiController, Route("sets")]
+public class SetController : ControllerBase
 {
-    [ApiController, Route("sets")]
-    public class SetController : ControllerBase
+    public SetController(
+        SetReadService setReadService,
+        ValidationResult validationResult)
     {
-        public SetController(
-            SetReadService setReadService,
-            ValidationResult validationResult)
-        {
-            SetReadService = setReadService;
-            ValidationResult = validationResult;
-        }
+        SetReadService = setReadService;
+        ValidationResult = validationResult;
+    }
 
-        public SetReadService SetReadService { get; }
-        public ValidationResult ValidationResult { get; }
+    public SetReadService SetReadService { get; }
+    public ValidationResult ValidationResult { get; }
 
-        [HttpGet, Route("{setName}"), Authorize(Roles = "lexica")]
-        public async Task<IActionResult> Get(string setName)
+    [HttpGet, Route("{setName}"), Authorize(Roles = "lexica")]
+    public async Task<IActionResult> Get(string setName)
+    {
+        OperationResult<Set?> operationResult = await SetReadService.GetAsync(setName);
+        if (!operationResult.Result)
         {
-            OperationResult<Set?> operationResult = await SetReadService.GetAsync(setName);
-            if (!operationResult.Result)
-            {
-                return BadRequest(ValidationResult.Errors);
-            }
-            return Ok(operationResult.Data);
+            return BadRequest(ValidationResult.Errors);
         }
+        return Ok(operationResult.Data);
+    }
 
-        [HttpGet, Authorize(Roles = "lexica")]
-        public async Task<IActionResult> Get()
+    [HttpGet, Authorize(Roles = "lexica")]
+    public async Task<IActionResult> Get()
+    {
+        OperationResult<List<Set>> operationResult = await SetReadService.GetAsync();
+        if (!operationResult.Result)
         {
-            OperationResult<List<Set>> operationResult = await SetReadService.GetAsync();
-            if (!operationResult.Result)
-            {
-                return BadRequest(ValidationResult.Errors);
-            }
-            return Ok(operationResult.Data);
+            return BadRequest(ValidationResult.Errors);
         }
+        return Ok(operationResult.Data);
     }
 }
