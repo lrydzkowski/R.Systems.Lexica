5de137feb6f542307d22d5ee93cb62a0fe3c5509
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2022-12-24 19:46:15 +0100

    test: correct problems with running tests

--- R.Systems.Lexica.Tests.Api.Web.Integration/Common/WebApplication/WebApiFactory.cs
+++ R.Systems.Lexica.Tests.Api.Web.Integration/Common/WebApplication/WebApiFactory.cs
@@ -1,8 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using R.Systems.Lexica.Api.Web;
+using R.Systems.Lexica.Tests.Api.Web.Integration.Options.AzureAd;
+using R.Systems.Lexica.Tests.Api.Web.Integration.Options.AzureFiles;
 
 namespace R.Systems.Lexica.Tests.Api.Web.Integration.Common.WebApplication;
 
 public class WebApiFactory : WebApplicationFactory<Program>
 {
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(
+            (_, configBuilder) =>
+            {
+                configBuilder.AddInMemoryCollection(new AzureAdOptionsData().ConvertToInMemoryCollection());
+                configBuilder.AddInMemoryCollection(new AzureFilesOptionsData().ConvertToInMemoryCollection());
+            }
+        );
+    }
 }
